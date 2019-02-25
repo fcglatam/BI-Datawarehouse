d7e0f955daf547a026070e540303d534f458f3b6
Diego <dvillamil+github@gmail.com>
2019-02-25 11:18:24 -0600

    Cambios en procedimiento general.

--- scripts-py/dv_1_inventory_daily.py
+++ scripts-py/dv_1_inventory_daily.py
@@ -4,11 +4,18 @@ Created on Thu Feb  7 17:23:26 2019
 @author: Diego, basado en script de Quique. 
 """
 
-# 0. Preparación
-# 1. Procedimiento digerido.
+import sys
+from os import getcwd, path, pardir
 
-%load_ext autoreload
-%autoreload 2
+if sys.stdin.isatty():
+    this_dir = path.dirname(path.realpath(__file__))
+    activate_this = path.join(
+        this_dir, pardir, "venv3", "bin", "activate_this.py")
+    exec(open(activate_this).read(), {'__file__': activate_this})
+else:
+    this_dir = getcwd()
+    %load_ext autoreload
+    %autoreload 2
 
 
 #%% Paquetes y configuración. 
@@ -20,19 +27,23 @@ from tools.base import local_engine, reflect_engine
 
 #%% 1. Procedimiento digerido. 
 
-yesterday = dt.today() - delta(1)
+which_day = dt.today()  # - delta(1)
 
 engine_ms = local_engine("mssql")
 meta_ms = reflect_engine(engine_ms, update = False)
 
 inventory_ms = ms.get_inventory(engine_ms, meta_ms)
-inventory_pg = ms.convert_inventory(inventory_ms, for_day = yesterday)
+inventory_pg = ms.convert_inventory(inventory_ms, for_day = which_day)
 
 engine_pg = local_engine("postgresql")
 inventory_pg.to_sql("xinventoryDaily", con = engine_pg, 
     if_exists = "append", index = False)
 
-inventory_pg.to_csv("../data/history/{}_inventory.csv".format(
+out_file = path.join(path.join(this_dir, pardir, "history"
+
+))
+inventory_pg.to_csv(
+    "../data/history/{}_inventory.csv".format(
     for_day.strftime("%y%m%d")), index = False) 
 
 
--- scripts-py/tools/models_beam.py
+++ scripts-py/tools/models_beam.py
@@ -5,10 +5,10 @@ p = beam.Pipeline(options=PipelineOptions())
 
 
 class MyOptions(PipelineOptions):
-  
   @classmethod
   def _add_argparse_args(self, parser):
     parser.add_argument('--input')
     parser.add_argument('--output')
     
-    
+
+lines = p | 'ReadMyFile' >> beam.io.ReadFromText('gs://some/inputData.txt')

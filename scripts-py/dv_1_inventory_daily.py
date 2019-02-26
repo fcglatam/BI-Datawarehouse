212ffa14aa3839a1dc5b6947a847c68f4329c6a1
Diego Villamil <diego.villamil@fcglatam.com>
2019-02-25 19:35:34 -0600

    cambios para el servidor

--- scripts-py/dv_1_inventory_daily.py
+++ scripts-py/dv_1_inventory_daily.py
@@ -4,18 +4,19 @@ Created on Thu Feb  7 17:23:26 2019
 @author: Diego, basado en script de Quique. 
 """
 
-import sys
+import __main__ as main
 from os import getcwd, path, pardir
+from IPython import get_ipython
 
-if sys.stdin.isatty():
+if hasattr(main, "__file__"):
     this_dir = path.dirname(path.realpath(__file__))
     activate_this = path.join(
         this_dir, pardir, "venv3", "bin", "activate_this.py")
     exec(open(activate_this).read(), {'__file__': activate_this})
 else:
     this_dir = getcwd()
-    %load_ext autoreload
-    %autoreload 2
+    get_ipython().run_line_magic('load_ext', 'autorealod')
+    get_ipython().run_line_magic('autoreload', '2')
 
 
 #%% Paquetes y configuración. 
@@ -36,10 +37,10 @@ inventory_ms = ms.get_inventory(engine_ms, meta_ms)
 inventory_pg = ms.convert_inventory(inventory_ms, for_day = which_day)
 
 engine_pg = local_engine("postgresql")
-# inventory_pg.to_sql("xinventoryDaily", con = engine_pg, 
-#    if_exists = "append", index = False)
+inventory_pg.to_sql("xinventoryDaily", con = engine_pg, schema = "public",
+   if_exists = "append", index = False)
 
-out_file = path.join(this_dir, pardir, "history", 
+out_file = path.join(this_dir, pardir, "data", "history", 
     "{}_inventory.csv".format(which_day.strftime("%y%m%d")) )
 
 inventory_pg.to_csv(out_file, index = False) 
